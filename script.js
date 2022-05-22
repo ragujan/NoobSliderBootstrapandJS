21e6de481950017ac1c35b98fa17287615feb7d1
unknown <stiflerwedontgiveup@gmail.comm>
2022-05-22 18:11:08 +0530

    withproducts

--- script.js
+++ script.js
@@ -6,55 +6,77 @@ const dgebCName = (cname) => {
   const selectedelement = document.querySelectorAll("." + cname);
   return selectedelement;
 };
-let getWindowSizeFunction = () => {
+let getWindowWidthFunction = () => {
   let windowWidthSize;
-  
-    windowWidthSize = window.outerWidth;
-    if (windowWidthSize == 588 || windowWidthSize < 588) {
-      console.log("YEAH its smaller")
-    }
-  console.log(windowWidthSize + "HEY");
+  windowWidthSize = window.innerWidth;
   return windowWidthSize;
-  
 };
-
 
 const popCircleRight = dgebi("popNavigateCircleRight");
 const popCircleLeft = dgebi("popNavigateCircleLeft");
 const popImagesContainer = dgebCName("popHotSliderImageContainer")[0];
-let clonedImage;
 const popImages = [...dgebCName("popHotSliderImage")];
+let clonedImage;
 let popCount = 0;
-let oglength = popImages.length;
-console.log(oglength);
 
-popCircleRight.addEventListener("click", () => {
-  console.log(popImages.length + "image slider length");
-  const popHotSliderImagesAgain = dgebCName("popHotSliderImage");
-  popCount++;
-  let windowSizeWidth = getWindowSizeFunction();
-   console.log(windowSizeWidth)
-  if ( windowSizeWidth== 588 || windowSizeWidth < 588 ) {
-    if (popCount == popImages.length) {
-      
-      console.log(popHotSliderImagesAgain[popHotSliderImagesAgain.length - 1]);
-      clonedImage = popImages[0].cloneNode(true);
-      popImagesContainer.appendChild(clonedImage);
-      popCount = 0;
-      popImagesContainer.style.transition = "none";
-    } else {
-      popImagesContainer.style.transition = "transform 0.5s linear";
+let bootstrapGridRanges = [576, 768, 992, 1200];
+let arrayPusher = (s, numOfSlidersperWidnowArray) => {
+  let st;
+  s.includes("-") ? st = s.replace("-", "") : st = s
+
+  let numOfSlidersperWidnow = 12 / st;
+
+  numOfSlidersperWidnowArray.push(numOfSlidersperWidnow - 1);
+};
+let decideMinusPoint = () => {
+  let numOfSlidersperWidnowArray = [];
+  let popImageArrayList = [];
+  let arrayClassLists = popImages[0].classList;
+  arrayClassLists.forEach((cl) => {
+    popImageArrayList.push(cl);
+  });
+
+  popImageArrayList.splice(0, 1);
+  popImageArrayList.forEach((colsize, id) => {
+    let s = colsize.split(/((col-).[a-z])|(col-)/);
+
+    if (s[1] == undefined || s[3] == "col-") {
+      arrayPusher(s[4], numOfSlidersperWidnowArray);
+    } else if (s[1] == "col-sm" && s[3] == undefined) {
+      arrayPusher(s[4], numOfSlidersperWidnowArray);
+    } else if (s[1] == "col-md" && s[3] == undefined) {
+      arrayPusher(s[4], numOfSlidersperWidnowArray);
+    } else if (s[1] == "col-lg" && s[3] == undefined) {
+      arrayPusher(s[4], numOfSlidersperWidnowArray);
     }
+  });
+  return numOfSlidersperWidnowArray;
+};
+
+let arrayofSliderPerWindowsMinus = [];
+arrayofSliderPerWindowsMinus = decideMinusPoint();
+let imageMoveandCloneFunction = (arrayMinusValue) => {
+  if (popCount == popImages.length - arrayMinusValue) {
+    clonedImage = popImages[0].cloneNode(true);
+    popImagesContainer.appendChild(clonedImage);
+    popCount = 0;
+    popImagesContainer.style.transition = "none";
   } else {
-    if (popCount == popImages.length - 1) {
-      console.log(popHotSliderImagesAgain[popHotSliderImagesAgain.length - 1]);
-      clonedImage = popImages[0].cloneNode(true);
-      popImagesContainer.appendChild(clonedImage);
-      popCount = 0;
-      popImagesContainer.style.transition = "none";
-    } else {
-      popImagesContainer.style.transition = "transform 0.5s linear";
-    }
+    popImagesContainer.style.transition = "transform 0.5s linear";
+  }
+};
+popCircleRight.addEventListener("click", () => {
+  popCount++;
+  let windowSizeWidth = getWindowWidthFunction();
+
+  if (windowSizeWidth > 0 && windowSizeWidth < 576) {
+    imageMoveandCloneFunction(arrayofSliderPerWindowsMinus[0]);
+  } else if (windowSizeWidth >= 576 && windowSizeWidth < 768) {
+    imageMoveandCloneFunction(arrayofSliderPerWindowsMinus[1]);
+  } else if (windowSizeWidth >= 768 && windowSizeWidth < 992) {
+    imageMoveandCloneFunction(arrayofSliderPerWindowsMinus[2]);
+  } else if (windowSizeWidth >= 992) {
+    imageMoveandCloneFunction(arrayofSliderPerWindowsMinus[3]);
   }
 
   if (popCount === 0) {
@@ -69,26 +91,21 @@ popCircleRight.addEventListener("click", () => {
   popImagesContainer.style.transform =
     "translateX(-" + singleImageWidth * popCount + "px)";
 });
+
 popCircleLeft.addEventListener("click", () => {
   if (popCount == 0) {
     popCount = 1;
   }
   popCount--;
 
-  console.log(popCount);
+  let singleImageWidth = popImages[0].clientWidth;
+  console.log(singleImageWidth);
+  popImagesContainer.style.transform =
+    "translateX(-" + singleImageWidth * popCount + "px)";
+});
+const getWidthBtn = dgebi("getWidthBtn");
+getWidthBtn.addEventListener("click", () => {
+  console.log(popImages[0].classList);
 
-  let popImageLength = popImages[0].clientWidth;
-  console.log(popImageLength);
-  let transferLength = popCount * popImageLength * 1;
-  let totalSliderLength = popImageLength * popImages.length;
-  console.log(transferLength);
-
-  console.log(totalSliderLength);
-
-  popImagesContainer.style.transform = "translateX(-" + transferLength + "px)";
-  popImagesContainer.style.transition = "transform 0.5s linear";
-  popImagesContainer.insertBefore(
-    popImages[popImages.length - 1],
-    popImagesContainer.firstChild
-  );
+  console.log(decideMinusPoint());
 });
--- test.js
+++
@@ -1,9 +0,0 @@
-
-const btn  = document.querySelector("#btn");
-
-btn.addEventListener('click',()=>{
-    const boxes = document.querySelectorAll(".abc");
-    let l = boxes.length;
-    console.log(l)
-    boxes[l-1].remove();
-})
